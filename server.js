711eea2253c3cb10d5824800b735a7849bec309a
Jed Smit <jedsmit1@gmail.com>
2020-02-09 16:11:40 -0800

    connect mongoose

--- server.js
+++ server.js
@@ -1,25 +1,32 @@
 const express = require("express");
+const logger = require("morgan");
 const mongoose = require("mongoose");
+
 const axios = require("axios");
 const cheerio = require("cheerio");
 
-const db = require("models");
+const db = require("./models");
 
 const PORT = 3000;
 
 const app = express();
 
+app.use(logger("dev"));
 app.use(express.urlencoded({ extended: true }));
-app, use(express.json());
+app.use(express.json());
 
 app.use(express.static("public"));
 
 // Set Handlebars
-var exphbs = require("express-handlebars");
+// var exphbs = require("express-handlebars");
 
-app.engine("handlebars", exphbs({ defaultLayout: "main" }));
-app.set("view engine", "handlebars");
+// app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+// app.set("view engine", "handlebars");
 
 
 // Connect to Mongo DB
-mongoose.connect("mongodb://localhost/scraper", { useNewUrlParser: true })
+mongoose.connect("mongodb://localhost/scraper", { useNewUrlParser: true });
+
+app.listen(PORT, function () {
+    console.log("App runnung on port " + PORT + "!");
+});
